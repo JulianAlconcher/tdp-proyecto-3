6dd6e0179bcdf5dde0fa2164a12fcb2045c576ad
Julian Alconcher <111883752+JulianAlconcher@users.noreply.github.com>
2022-10-31 16:51:07 -0300

    Prueba grafica de plantas

--- src/GUI/GUI.java
+++ src/GUI/GUI.java
@@ -12,9 +12,15 @@ import Logica.Logica;
 import java.awt.Color;
 import java.awt.Dimension;
 
+import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import java.awt.GridLayout;
+import java.awt.Image;
+
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import javax.swing.border.Border;
 
 public class GUI implements Runnable{
 
@@ -25,6 +31,7 @@ public class GUI implements Runnable{
 	protected Logica miLogica;
 	protected CeldaGrafica matrizGrafica[][];
 	protected ImageIcon imagenBoton;
+	protected ImageIcon imagenPortadaMenu;
 	protected MouseHandler miMouse;
 
 	/**
@@ -43,19 +50,26 @@ public class GUI implements Runnable{
 		frame.setBounds(100, 100, 961, 547);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
-		frame.setResizable(false);
+		frame.setResizable(true);
 		frame.getContentPane().setLayout(new CardLayout(0, 0));
 
 		panelMenu = new JPanel();
-		panelMenu.setBackground(Color.RED);
+		panelMenu.setBackground(Color.BLACK);
 		frame.getContentPane().add(panelMenu);
 		panelMenu.setLayout(null);
+		
 
 		btnJugar = new JButton("Jugar");
-		btnJugar.setBounds(389, 215, 157, 59);
+		btnJugar.setBounds(391, 409, 182, 77);
 		imagenBoton = new ImageIcon(this.getClass().getResource("/Images/BotonJugarPvZ.jpg"));
 		btnJugar.setIcon(imagenBoton);
 		panelMenu.add(btnJugar);
+		
+		JLabel lblImagenMenu = new JLabel("");
+		lblImagenMenu.setBounds(0, 0, 947, 510);
+		panelMenu.add(lblImagenMenu);
+		imagenPortadaMenu = new ImageIcon(this.getClass().getResource("/Images/portada.png"));
+		lblImagenMenu.setIcon(imagenPortadaMenu);
 
 		panelJuego = new JPanel();
 		panelJuego.setBackground(Color.PINK);
@@ -82,10 +96,11 @@ public class GUI implements Runnable{
 		for(int i = 0; i < miLogica.getFilas(); i++) {
 			for(int j = 0; j < miLogica.getColumnas(); j++) {
 				CeldaGrafica cg = new CeldaGrafica();
-				if(i%2 == (j%2)) //Esto es una flasheada, no le den bola.
-					cg.setIcon(cg.getImagen());
-				else
-					cg.setIcon(new ImageIcon(this.getClass().getResource("/Images/Fondo2.png")));
+				cg.setIcon(cg.getImagen());
+				if(i==4 && j==0) {
+					ImageIcon im = new ImageIcon(this.getClass().getResource("/Images/nuez.gif")); 
+					cg.setIcon(im);
+				}
 				panelJuego.add(cg);
 			}
 		}
